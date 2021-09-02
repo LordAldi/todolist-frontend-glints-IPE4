b56baa9ca9fc6dfd4fc7e3425ffc5b78e6e951eb
LordAldi <aldianugra09@gmail.com>
2021-09-02 22:22:37 +0700

    initialize project structure

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BackHome from "../components/BackHome";
+
+const Login = () => {
+  return (
+    <div>
+      Login
+      <BackHome />
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BackHome from "../components/BackHome";
+
+const Register = () => {
+  return (
+    <div>
+      Register
+      <BackHome />
+    </div>
+  );
+};
+
+export default Register;
