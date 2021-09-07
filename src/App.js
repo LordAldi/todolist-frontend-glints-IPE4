bfb6fa2eddeb5e832f3e811e2114d6e9aaed3c25
jember08 <afandayul@gmail.com>
2021-09-08 00:04:03 +0700

    review scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import './styles/index.scss'
 
 
 import SideMenu from './components/SideMenu';
